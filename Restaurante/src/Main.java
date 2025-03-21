dda57004dc8e0c3a6b9f9e7dd3adf956fbbe2713
Ricardo-LK <92482482+Ricardo-LK@users.noreply.github.com>
2025-03-21 09:59:29 -0300

    Update Main.java

--- Restaurante/src/Main.java
+++ Restaurante/src/Main.java
@@ -5,27 +5,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Restaurante restaurante = new Restaurante();
+        restaurante.addPratoDisponivel(new Prato("Lasanha", 2.00f));
+        restaurante.addPratoDisponivel(new Prato("Pizza", 2.00f));
+        restaurante.addPratoDisponivel(new Prato("Hamburger", 2.00f));
+        restaurante.addPratoDisponivel(new Prato("Coquinha", 2.00f));
+        restaurante.addPratoDisponivel(new Prato("Stogonoff", 2.00f));
 
         System.out.println("Nome cliente: ");
         String nomeCliente = scan.next();
         System.out.println("Numero telefone: ");
         String telefoneCliente = scan.next();
-
         System.out.println("\n");
 
         Cliente cliente = new Cliente(nomeCliente, telefoneCliente);
-
         Pedido pedido = new Pedido(cliente);
 
-        pedido.addPrato(new Prato("Lasanha", 2.00f));
-        pedido.addPrato(new Prato("Pizza", 2.00f));
-        pedido.addPrato(new Prato("Hamburger", 2.00f));
-        pedido.addPrato(new Prato("Coquinha", 2.00f));
-        pedido.addPrato(new Prato("Stogonoff", 2.00f));
-        
-        pedido.showPedido();
+        boolean running = true;
+        int opcao = 0;
+        while (running)
+        {
+            System.out.println("(1): ");
+            opcao = scan.nextInt();
+            System.out.println("\n");
 
-        System.out.println("\n");
-        System.out.println("Preco total: " + pedido.getPrecoTotal() + "R$");
+            if (opcao == 1)
+            {
+
+            }
+
+            System.out.println("\n");
+            System.out.println("Preco total: " + pedido.getPrecoTotal() + "R$");
+        }
     }
 }
+
